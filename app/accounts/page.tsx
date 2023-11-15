7cc307201971af7538dd26df231eb0a3bcd27907
Ovodo <ovdizzle@gmail.com>
2023-11-15 22:34:54 +0100

    supposed to be fixed

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { User } from "next-auth";
 import { baseUrl } from "@/config";
 import Accounts from "@/components/Accounts";
+import axios from "axios";
 
 const getUsers = async () => {
   const res = await fetch(`${baseUrl}/api/users`);
@@ -12,13 +13,23 @@ const getUsers = async () => {
   const data = await res.json();
   return data;
 };
+const getUser = async () => {
+  const res = await axios.get(`${baseUrl}/api/users`);
+  if (res.statusText !== "OK") {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  const data = res.data;
+  return data;
+};
 
 const Page = async () => {
   const users: User[] = await getUsers();
+  const use = await getUser();
 
-  console.log("users", users);
+  console.log("use", use);
 
-  return <Accounts data={users} />;
+  return <Accounts data={use} />;
 };
 
 export default Page;
